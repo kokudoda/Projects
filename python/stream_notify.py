c6b20d39edc166156b62f790d0d90d03ae06c467
Your Name <you@example.com>
2026-03-28 17:10:28 +0900

    Twitch APIを利用した、配信者が配信を開始すると通知するアプリ

--- python/stream_notify.py
+++ python/stream_notify.py
@@ -1,143 +1,132 @@
-# APIを利用して配信者が配信を開始すると通知を出すコード
-
 import requests
+from plyer import notification  #通知機能
 import time
-from plyer import notification   #通知を出すため
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # .envファイルを読み込みむ
+load_dotenv()   #.envファイルの読み込み
 
-CLIENT_ID = os.getenv("CLIENT_ID")                  #.envから取得
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")          #.envから取得
-target_channel = "kato_junichi0817"                 #目的のチャンネル
-CHECK_INTARVAL = 60                                 #60秒ごとにチェック
+CLIENT_ID = os.getenv("CLIENT_ID")      #自分のクライアントID
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")      #自分のクライアントシークレット
+Target = "kato_junichi0817"     #配信者
 
+CHECK_INTARVAL = 60            #60秒ごとにチェック
 
-#アクセストークンを取得する関数
-def get_access_token():
-    url = "https://id.twitch.tv/oauth2/token"  #twitchサーバーのurl トークンを貰うためのurl
+img = os.getenv("IMG")  #send_notification()で出す通知のアイコン画像
 
-    #Twitchサーバーに送る情報を辞書でまとめる
-    data = {
+#トークンを取得する関数
+def get_token():
+    url = "https://id.twitch.tv/oauth2/token"   #目的のurl
+    
+    """ <POSTリクエストの例↓>
+    client_id=hof5gwx0su6owfnys0yan9c87zr6t         クライアントid
+    &client_secret=41vpdji4e9gif29md0ouet6fktd2     クライアントシークレット
+    &grant_type=client_credentials                  grant_type
+    """
+
+    data = {    #↑を参考に
         "client_id" : CLIENT_ID,
         "client_secret" : CLIENT_SECRET,
-        "grant_type" : "client_credentials" #アプリ同士の通信で必要
+        "grant_type" : "client_credentials"
     }
-
     response = requests.post(url,data=data)
+    result = response.json()
 
-    #print(response.text)  #デバッグ用
-    result = response.json()    #返答をpython辞書に変換
-    return  result["access_token"]
-""""
-レスポンスで帰ってくる値
-{
-  "access_token": "jostpf5q0uzmxmkba9iyug38kjtgh",
-  "expires_in": 5011271,
-  "token_type": "bearer"
-}
+    """"
+    レスポンスで帰ってくる値
+    {
+    "access_token": "xxxxxxxxxxxxxxxxxxx",
+    "expires_in": xxxxxxx,
+    "token_type": "bearer"
+    }
+    """
 
-"""
-#配信しているかどうか確認する関数
+    return result["access_token"]
+
+    
+#配信しているかどうか調べる関数
 def is_live(access_token):
+    url = "https://api.twitch.tv/helix/streams"     #Get Streamsのリクエスト先
 
-    #配信状況を問い合わせるTwitchサーバーのurl
-    url = "https://api.twitch.tv/helix/streams"      
+    """ <getリクエストの例↓>
+    curl -X GET
+        'https://api.twitch.tv/helix/streams?user_login=afro&user_login=cohhcarnage&user_login=lana_lux' \
+        -H 'Authorization: Bearer xxxxxxxxxxxxxxxxx' \
+        -H 'Client-Id: xxxxxxxxxxxxxxxxxxxxxx'
+    """
 
-    #認証済みである情報
-    headers = {
-        "Client-id" : CLIENT_ID,
-        "Authorization" : f"Bearer {access_token}"   #Beare認証
+    headers = {     #-H なのでヘッダー(headers)
+        "Authorization" : f"Bearer {access_token}",     #Beare認証
+        "Client-Id" : CLIENT_ID     #自分のid
+    }
+    
+    params = {      # ?user_loginなのでパラメータ(params)
+        "user_login" : Target,      #調べたい配信者
     }
 
-    #誰の配信状況をしらべたいか
-    parameter = {"user_login" : target_channel}
+    response = requests.get(url,params=params,headers=headers)
+    if response.status_code != 200:
+        print("Error")
+        return False,None,None,None
+    
+    #print(response.status_code)
+    res_json = response.json()
+    #print(res_json)
+    if  res_json["data"]:    #"data" : []が空の時は配信はしていない
+        stream_title = res_json["data"][0]["title"]            #配信タイトル
+        game_name = res_json["data"][0]["game_name"]           #ゲーム名
+        start_stream = res_json["data"][0]["started_at"]       #配信開始時刻
 
-    #サーバーに問い合わせる(リクエスト).get(問い合わせ先, 認証情報, 誰の配信状況か)
-    response = requests.get(url,headers=headers,params=parameter)
-    """
-    getの引数(url,)
-    curl -X GET
-    'https://api.twitch.tv/helix/streams?user_login=afro&user_login=cohhcarnage&user_login=lana_lux' \
-    -H 'Authorization: Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx' \
-    -H 'Client-Id: uo6dggojyb8d6soh92zknwmi5ej1q2'
-    """
-    data = response.json()
-
-    #配信していたらTrueを返す
-    if len(data["data"]) > 0:
-        title = data["data"][0]["title"]
-        return True,title  #配信中である(True)とタイトルを返す
+        return True,stream_title,game_name,start_stream
     else:
-        return False,""     #配信なし(False)と空文字を返す
-    """
-    配信していないときは↓が空("data":[],)となる.
-    {
-  "data": [
-    {
-      "id": "40952121085",
-      "user_id": "101051819",
-      "user_login": "afro",
-      ...
-    },
-    ...
-  ],
-  "pagination": {}
-}
-    """
+        return False,None,None,None
+    
 
-
-
-#通知を出す関数
-def send_notifiation(title,message):
+#ポップアップで通知する関数
+def send_notification(stream_title,game_name):
     notification.notify(
-        message = message,
-        title = title,
+        title = stream_title,
+        message = game_name,
+        app_icon = img,
         timeout = 10
     )
 
-
 def main():
-    print("Twitchうんこちゃん通知アプリ")
-    print(f"{CHECK_INTARVAL}秒ごとに配信をチェック")
-    print("終了するには Ctrl+C を押してください\n")
-    
-    #アクセストークンの取得
-    access_token = get_access_token()
-    print("アクセストークンの取得に成功しました")
 
-    was_live = False
+    print("うんこちゃん通知アプリです\n")
+    print("終了は Ctrl+C です")
+
+
+    access_token = get_token()
+    print("トークンの取得に成功しました")
+
+    was_live = False    #通知したかどうかのフラグ(通知済み:True , 未通知:False)
 
     while True:
         try:
-            live,title= is_live(access_token)
+            live,stream_title,game_name,start_stream = is_live(access_token)
 
-            if live and not was_live:
-                print("配信を開始しました")
-                send_notifiation(
-                    message = "うんこちゃんが配信を開始しました",
-                    title = f"{title}"
+            if live and not was_live:   #liveがTrueかつ未通知のとき
+                print(f"配信開始 : {start_stream}")
+                send_notification(
+                    stream_title,
+                    game_name
                 )
-                was_live = True
-            
-            elif not live and was_live:
-                print("配信が終了しました")
-                was_live = False
+                was_live = True     #通知したのでTrueにする
 
-            else:
-                status = "配信中です" if live else "配信していません"
-                print(f"[{status}]")
+            if not live:
+                print("配信していません")
+                live = False
 
             time.sleep(CHECK_INTARVAL)
 
-        except KeyboardInterrupt:
+        except KeyboardInterrupt:   
             print("\n終了します")
             break
 
         except Exception as e:
             print(f"エラーが発生しました : {e}")
-            print("60秒後に再試行します")
+            print("60秒後に再開します")
             time.sleep(60)
 
 if __name__ == "__main__":
